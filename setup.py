14634870e917e33dde00b69831ab82121b41cb3b
fritshermans <post@fritshermans.nl>
2021-04-11 20:40:46 +0200

    adds missing packages to setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name='DedupliPy',
       version='0.1',
       packages=find_packages('deduplipy'),
       package_dir={'': 'deduplipy'},
-      install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn', 'networkx'])
+      install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn', 'networkx', 'fuzzywuzzy', 'modAL'])
