3b4d8112b3a80561417743728fa44e89a5a5aeb6
fritshermans <post@fritshermans.nl>
2021-04-16 21:30:41 +0200

    Revert "removes commas from chicago data"
    This reverts commit aeda53c2

--- notebooks/testscript.py
+++ notebooks/testscript.py
@@ -8,9 +8,7 @@ from deduplipy.deduplicator import Deduplicator
 if __name__ == "__main__":
     df_train = load_data(kind='childcare', return_pairs=False)
 
-    myDedupliPy = Deduplicator('name_address', 30)
+    myDedupliPy = Deduplicator('name_address', 10)
 
     myDedupliPy.fit(df_train)
-    res = myDedupliPy.predict(df_train)
-    res.to_csv('res.csv', sep="|")
-    print(res)
+    print(myDedupliPy.predict(df_train))
