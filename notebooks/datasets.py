047c767271ad8ea9154320d15b935f646fb526c7
fritshermans <post@fritshermans.nl>
2021-04-16 20:53:47 +0200

    drop duplicates and strip in chicago data

--- notebooks/datasets.py
+++ notebooks/datasets.py
@@ -51,7 +51,8 @@ def load_stoxx50(return_pairs=False):
 def load_chicago_childcare(return_pairs=False):
     df = pd.read_csv('data/csv_example_input_with_true_ids.csv')
     df['name_address'] = df['Site name'] + " " + df['Address']
-    df['name_address'] = df['name_address'].str.lower()
+    df['name_address'] = df['name_address'].str.lower().str.strip()
+    df = df.drop_duplicates(subset=['name_address'])
     if return_pairs:
         size = 10_000
 
