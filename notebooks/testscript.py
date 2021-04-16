aeda53c2e4a97b08714b6cb19705eadf8b165c49
fritshermans <post@fritshermans.nl>
2021-04-16 21:29:16 +0200

    removes commas from chicago data

--- notebooks/testscript.py
+++ notebooks/testscript.py
@@ -8,7 +8,9 @@ from deduplipy.deduplicator import Deduplicator
 if __name__ == "__main__":
     df_train = load_data(kind='childcare', return_pairs=False)
 
-    myDedupliPy = Deduplicator('name_address', 10)
+    myDedupliPy = Deduplicator('name_address', 30)
 
     myDedupliPy.fit(df_train)
-    print(myDedupliPy.predict(df_train))
+    res = myDedupliPy.predict(df_train)
+    res.to_csv('res.csv', sep="|")
+    print(res)
