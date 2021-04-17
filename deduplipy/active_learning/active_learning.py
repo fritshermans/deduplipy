22d9ac47cf44a88b724043a0eff4c134f82c40a5
fritshermans <post@fritshermans.nl>
2021-04-17 20:38:21 +0200

    prints score histogram

--- deduplipy/active_learning/active_learning.py
+++ deduplipy/active_learning/active_learning.py
@@ -91,6 +91,13 @@ class ActiveStringMatchLearner(BaseEstimator):
                     break
             learn_counter += 1
 
+        # print score histogram
+        probas = self.learner.predict_proba(sample_combinations_array)[:,1]
+        count, division = np.histogram(probas, bins=np.arange(0,1.01,0.05))
+        hist = pd.DataFrame({'count':count, 'division':division[1:]})
+        print(hist)
+
+
     def predict(self, X):
         return self.learner.predict(X)
 
