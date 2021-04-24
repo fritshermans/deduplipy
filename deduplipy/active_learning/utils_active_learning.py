dfb5e6606888f6bf6d7f0f9ac07ab78b6c4cf2ec
fritshermans <post@fritshermans.nl>
2021-04-24 16:49:16 +0200

    adds docstring to utils_active_learning.py

--- deduplipy/active_learning/utils_active_learning.py
+++ deduplipy/active_learning/utils_active_learning.py
@@ -1,4 +1,14 @@
 def input_assert(message, choices) -> str:
+    """
+    Adds functionality to the python function `input` to limit the choices that can be returned
+
+    Args:
+        message: message to user
+        choices: list containing possible choices that can be returned
+
+    Returns:
+        input returned by user
+    """
     output = input(message).lower()
     if output not in choices:
         print('Wrong input!')
