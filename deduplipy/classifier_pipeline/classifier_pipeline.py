7f658b61b543051bab491ccc5acc9e46d75379da
fritshermans <post@fritshermans.nl>
2021-05-03 14:20:19 +0200

    adds docstrings to classifier_pipeline.py

--- deduplipy/classifier_pipeline/classifier_pipeline.py
+++ deduplipy/classifier_pipeline/classifier_pipeline.py
@@ -8,6 +8,14 @@ from sklearn.preprocessing import PolynomialFeatures
 
 class ClassifierPipeline(BaseEstimator):
     def __init__(self, interaction=False):
+        """
+        Classification pipeline to be used in ActiveStringMatchLearner. Does not throw an error when there is only one
+        class in the targets during the first steps in active learning.
+
+        Args:
+            interaction: Whether or not to include interaction features
+
+        """
         if interaction:
             self.classifier = make_pipeline(
                 StandardScaler(),
@@ -24,6 +32,18 @@ class ClassifierPipeline(BaseEstimator):
         self._fitted = True
 
     def fit(self, X, y):
+        """
+        Fit the classification pipeline. Does not throw an error when there is only one class in the targets during the
+        first steps in active learning.
+
+        Args:
+            X: features
+            y: target
+
+        Returns:
+            fitted instance
+
+        """
         # force the instance not to fit if there is only one class in y, needed for the first steps in active learning
         if len(set(y)) == 1:
             return self
@@ -32,7 +52,27 @@ class ClassifierPipeline(BaseEstimator):
             return self
 
     def predict(self, X):
+        """
+        Predict using fitted instance.
+
+        Args:
+            X: features
+
+        Returns:
+            predictions
+
+        """
         return self.classifier.predict(X)
 
     def predict_proba(self, X):
+        """
+        Predict probabilities using fitted instance.
+
+        Args:
+            X: features
+
+        Returns:
+            predicted probabilities
+
+        """
         return self.classifier.predict_proba(X)
