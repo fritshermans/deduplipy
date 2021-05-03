47b3952830a4b3b86804c8962255f2e8fb8acc1e
fritshermans <post@fritshermans.nl>
2021-05-03 14:20:50 +0200

    optimizes imports in classifier_pipeline.py

--- deduplipy/classifier_pipeline/classifier_pipeline.py
+++ deduplipy/classifier_pipeline/classifier_pipeline.py
@@ -1,9 +1,8 @@
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
 
 
 class ClassifierPipeline(BaseEstimator):
