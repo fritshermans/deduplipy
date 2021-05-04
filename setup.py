f7c712a678607dffe2c6d6449f83eb6b5f4e9c16
fritshermans <post@fritshermans.nl>
2021-05-04 16:26:25 +0200

    adds pyproject.toml and adjusts setup.py for pypi deployment

--- setup.py
+++ setup.py
@@ -25,8 +25,21 @@ docs_packages = [
 
 dev_packages = base_packages + util_packages + docs_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='DedupliPy',
       version='0.1',
+      author="Frits Hermans",
+      description="End-to-end deduplication solution",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/fritshermans/deduplipy",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
       packages=find_packages(exclude=['notebooks']),
       package_data={"deduplipy": ["data"]},
       install_requires=base_packages,
@@ -35,4 +48,5 @@ setup(name='DedupliPy',
           "dev": dev_packages,
           "docs": docs_packages,
       },
+      python_requires=">=3.7",
       )
