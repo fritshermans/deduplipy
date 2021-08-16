725ee737e90128d990b48866f5923cae072128c6
fritshermans <post@fritshermans.nl>
2021-08-16 08:14:38 +0200

    removes redundant fill_missing_links_diff_evol function

--- deduplipy/clustering/fill_missing_edges.py
+++ deduplipy/clustering/fill_missing_edges.py
@@ -7,46 +7,6 @@ from fancyimpute import SoftImpute
 from deduplipy.config import CONVERGENCE_THRESHOLD
 
 
-def _get_missing_positions(matrix):
-    pos_missing = []
-    rows, cols = np.triu_indices_from(matrix)
-    for row, col in zip(rows, cols):
-        if matrix[row, col] == 0:
-            pos_missing.append([row, col])
-    return pos_missing
-
-
-def fill_missing_links_diff_evol(matrix):
-    matrix_ = matrix.copy()
-    # we assume a correlation matrix:
-    np.fill_diagonal(matrix_, 1)
-
-    def helper_function(x):
-        for val, pos in zip(x, pos_missing):
-            matrix_[pos[0], pos[1]] = val
-            matrix_[pos[1], pos[0]] = val
-        eigenvalues = np.linalg.eigvals(matrix_)
-
-        return -min(eigenvalues)
-
-    pos_missing = _get_missing_positions(matrix)
-    if not len(pos_missing):
-        return matrix
-
-    bounds = [(0, 1) for _ in pos_missing]
-
-    res = differential_evolution(helper_function, bounds)
-
-    for val, pos in zip(res.x, pos_missing):
-        matrix_[pos[0], pos[1]] = val
-        matrix_[pos[1], pos[0]] = val
-
-    # the adjacency matrix needs to have zeros on the diagonal
-    np.fill_diagonal(matrix_, 0)
-
-    return matrix_
-
-
 def fill_missing_links(matrix, convergence_threshold=CONVERGENCE_THRESHOLD):
     """
     Fill missing values in adjacency matrix using SoftImpute. Missing values are considered to be zero,
