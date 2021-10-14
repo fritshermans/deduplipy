2e4e0d9f4f2c0184d870f33259eea680a1caad5f
fritshermans <post@fritshermans.nl>
2021-10-14 14:44:12 +0200

    bumps version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='DedupliPy',
-      version='0.5.3',
+      version='0.5.5',
       author="Frits Hermans",
       description="End-to-end deduplication solution",
       long_description=long_description,
