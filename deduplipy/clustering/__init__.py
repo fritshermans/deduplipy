67bd72763f12ec17cbaae18e0ed06d9c2d81626d
fritshermans <post@fritshermans.nl>
2021-10-23 14:48:01 +0200

    adds fill_missing_links to __init__.py in clustering module

--- deduplipy/clustering/__init__.py
+++ deduplipy/clustering/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ['hierarchical_clustering']
+__all__ = ['hierarchical_clustering', 'fill_missing_links']
 
 from .clustering import hierarchical_clustering
+from .fill_missing_edges import fill_missing_links
